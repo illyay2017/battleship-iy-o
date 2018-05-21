27f3a662269be44d4b6067f7734ada9c32bc862b
illyay2017 <illya.yanchuk@gmail.com>
2018-05-20 19:04:01 -0700

    trying to refactor to use static file serving

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const port = process.env.PORT || 8080;
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
+app.use(express.static(__dirname + '/../dist'))
 app.get('/', (req, res) => {
   res.send('Hello from BattleShip server');
 });
